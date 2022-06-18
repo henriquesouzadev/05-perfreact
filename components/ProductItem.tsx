666354d97b1f0736ea94328495e6a8b12b764dc5
henriquesouzadev <henriquesouzadev@gmail.com>
2022-06-18 15:43:56 -0300

    finalizando projeto

---
+++ components/ProductItem.tsx
@@ -0,0 +1,41 @@
+import { memo, useState } from 'react';
+import { AddProductToWishlistProps } from './AddProductToWishlist';
+import dynamic from 'next/dynamic';
+
+const AddProductToWishlist = dynamic<AddProductToWishlistProps>(() => {
+   return import('./AddProductToWishlist').then(mod => mod.AddProductToWishlist)
+}, {
+   loading: () => <span>Carregando...</span>
+});
+
+interface ProductItemProps {
+   product: {
+      id: number;
+      price: number;
+      priceFormatted: string;
+      title: string;
+   }
+   onAddToWishList: (id: number) => void;
+}
+
+function ProductItemComponent({ product, onAddToWishList }: ProductItemProps) {
+   const [isAddingToWishList, setIsAddingToWishList] = useState(false);
+
+   return (
+      <div>
+         {product.title} - <strong>{product.priceFormatted}</strong>
+         <button onClick={() => setIsAddingToWishList(true)}>Adicionar aos favoritos</button>
+
+         {isAddingToWishList && (
+            <AddProductToWishlist
+               onAddToWishList={() => onAddToWishList(product.id)}
+               onRequestClose={() => setIsAddingToWishList(false)}
+            />
+         )}
+      </div>
+   );
+}
+
+export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
+   return Object.is(prevProps.product, nextProps.product);
+});
---
+++ components/SearchResults.tsx
@@ -0,0 +1,62 @@
+import { List, ListRowRenderer } from 'react-virtualized';
+import { ProductItem } from "./ProductItem";
+
+interface SearchResultsProps {
+   totalPrice: number;
+   results: Array<{
+      id: number;
+      price: number;
+      priceFormatted: string;
+      title: string;
+   }>
+   onAddToWishList: (id: number) => void;
+}
+
+export function SearchResults({ totalPrice, results, onAddToWishList }: SearchResultsProps) {
+   const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+      return (
+         <div key={key} style={style}>
+            <ProductItem
+               product={results[index]}
+               onAddToWishList={onAddToWishList}
+            />
+         </div>
+      );
+   }
+
+   return (
+      <div>
+         <h2>{totalPrice}</h2>
+
+         <List
+            height={300}
+            rowHeight={30}
+            width={900}
+            overscanRowCount={5}
+            rowCount={results.length}
+            rowRenderer={rowRenderer}
+         />
+      </div>
+   );
+}
+
+/**
+ * 1. Criar uma nova versão do componente
+ * 2. Comparar com a versão anterior
+ * 3. Se houverem alterações, vai atualizar o que alterou
+ */
+
+/**
+ * 1. Pure Functional Components
+ * 2. Render too often
+ * 3. Re-renders with same props
+ * 4. Medium to big size
+ */
+
+/**
+ * useMemo / useCallback
+ * 
+ * 1. Cálculos pesados
+ * 2. Igualdade referencial (quando a gente repassa aquela informação a um componente filho)
+ * 
+ */
